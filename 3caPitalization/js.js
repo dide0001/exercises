0be71e8fe67942b822b5dcf90bfe6069da17f050
Diego <dide0001@stud.kea.dk>
2026-02-06 13:49:05 +0100

    done done

--- 3caPitalization/js.js
+++ 3caPitalization/js.js
@@ -1,4 +1,4 @@
 const peter = "peter"
 
-console.log(peter.substring(0,2).toLowerCase() + peter.substring(2,3).toUpperCase() + peter.substring(3).toLowerCase())
+console.log(peter.substring(0,2) + peter.substring(2,3).toUpperCase() + peter.substring(3))
 
