3079581ace612358abbc18f20983dd637b999610
Diego <dide0001@stud.kea.dk>
2026-02-06 13:54:08 +0100

    opgave 3 done

--- 3caPitalization/js.js
+++ 3caPitalization/js.js
@@ -1,4 +1,11 @@
-const peter = "peter"
+// opgave 2 3caPitalization
+
+const peter = "peter flot røv"
 
 console.log(peter.substring(0,2) + peter.substring(2,3).toUpperCase() + peter.substring(3))
+
+// opgave 3 Real capitalization
+console.log(peter.substring(0,1).toUpperCase() + peter.substring(1))
+
+
 
